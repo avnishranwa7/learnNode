cc8aec5225b2998f5cbf0ceb8a3a1b6bf471d1fc
Avnish Ranwa <avnishranwa7@gmail.com>
2023-07-28 17:42:34 +0530

    Updating and deleting post

--- restapi-project/controllers/feed.js
+++ restapi-project/controllers/feed.js
@@ -1,3 +1,6 @@
+const fs = require('fs');
+const path = require('path');
+
 const { validationResult } = require('express-validator');
 
 const Post = require('../models/post');
@@ -26,7 +29,7 @@ exports.createPost = (req, res, next) => {
         throw err;
     }
 
-    if(!req.file){
+    if (!req.file) {
         const error = new Error('No image provided');
         error.statusCode = 422;
         throw err;
@@ -80,4 +83,88 @@ exports.getPost = (req, res, next) => {
             }
             next(err);
         })
+}
+
+exports.updatePost = (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        const error = new Error('Validation failed, entered data is incorrect');
+        error.statusCode = 422;
+        throw err;
+    }
+
+    const postId = req.params.postId;
+    const title = req.body.title;
+    const content = req.body.content;
+    let imageUrl = req.body.image;
+
+    if (req.file) {
+        imageUrl = req.file.path.replace('\\', '/');
+    }
+
+    if (!imageUrl) {
+        const error = new Error('No image picked.');
+        error.statusCode = 422;
+        throw err;
+    }
+
+    Post.findById(postId)
+        .then(post => {
+            if (!post) {
+                const error = new Error('Could not find post');
+                error.statusCode = 404;
+                throw err;
+            }
+
+            if (imageUrl !== post.imageUrl) {
+                clearImage(post.imageUrl);
+            }
+
+            post.title = title;
+            post.imageUrl = imageUrl;
+            post.content = content;
+
+            return post.save();
+        })
+        .then(result => {
+            res.status(200).json({
+                message: 'Post updated',
+                post: result
+            });
+        })
+        .catch(err => {
+            if (!err.statusCode) {
+                err.statusCode = 500;
+            }
+            next(err);
+        })
+}
+
+exports.deletePost = (req, res, next) => {
+    const postId = req.params.postId;
+    Post.findById(postId)
+        .then(post => {
+            if (!post) {
+                const error = new Error('Could not find post');
+                error.statusCode = 404;
+                throw err;
+            }
+
+            clearImage(post.imageUrl);
+            return Post.findByIdAndRemove(postId);
+        })
+        .then(result => {
+            res.status(200).json({ message: 'Post deleted' });
+        })
+        .catch(err => {
+            if (!err.statusCode) {
+                err.statusCode = 500;
+            }
+            next(err);
+        })
+}
+
+const clearImage = filePath => {
+    filePath = path.join(__dirname, '..', filePath);
+    fs.unlink(filePath, err => console.log(err));
 }
--- restapi-project/routes/feed.js
+++ restapi-project/routes/feed.js
@@ -16,4 +16,14 @@ router.post('/post',
     feedController.createPost
 );
 
+router.put('/post/:postId',
+    [
+        body('title').trim().isLength({ min: 5 }),
+        body('content').trim().isLength({ min: 5 }),
+    ],
+    feedController.updatePost
+);
+
+router.delete('/post/:postId', feedController.deletePost);
+
 module.exports = router;
